6b7dd856958c22ddf698a67117b3e3fa7c4e74c8
Quemero94 <matiaserco@gmail.com>
2020-05-06 15:58:59 -0300

    ruteando error de auth solucionado

--- src/app/ruteando/ruteando.module.ts
+++ src/app/ruteando/ruteando.module.ts
@@ -21,65 +21,70 @@ import { QuienSoyComponent } from "../componentes/quien-soy/quien-soy.component"
 import { ListadoDePaisesComponent } from "../componentes/listado-de-paises/listado-de-paises.component";
 import { MapaDeGoogleComponent } from "../componentes/mapa-de-google/mapa-de-google.component";
 import { JugadoresListadoComponent } from "../componentes/jugadores-listado/jugadores-listado.component";
-import { AuthGuard } from "../guards/auth.guard";
+import { AuthGuardService } from '../guards/auth-guard.service';
+
+
+
 
 // declaro donde quiero que se dirija
 const MiRuteo = [
+  // Fuera del guard
+  { path: '', component: PrincipalComponent, data: { animation: 'Home' } },
+  { path: 'Login', component: LoginComponent, data: { animation: 'About' } },
+  { path: 'Registro', component: RegistroComponent, data: { animation: 'Contact' } },
+  { path: 'QuienSoy', component: QuienSoyComponent, data: { animation: 'Contact' } },
+  // Rutas protegias, debe estar logeado
   {
-    path: "Jugadores",
-    component: JugadoresListadoComponent,
-    canActivate: [AuthGuard]
+    path: 'Mapa', component: MapaDeGoogleComponent,
+    canActivate: [AuthGuardService],
+    data: { animation: 'Contact' }
   },
-  { path: "", component: PrincipalComponent, canActivate: [AuthGuard] },
-  { path: "Login", component: LoginComponent },
-  { path: "Mapa", component: MapaDeGoogleComponent, canActivate: [AuthGuard] },
-  { path: "QuienSoy", component: QuienSoyComponent, canActivate: [AuthGuard] },
-  { path: "Registro", component: RegistroComponent },
   {
-    path: "Principal",
-    component: PrincipalComponent,
-    canActivate: [AuthGuard]
+    path: 'Principal', component: PrincipalComponent,
+    canActivate: [AuthGuardService],
+    data: { animation: 'Contact' }
   },
-  { path: "Listado", component: ListadoComponent, canActivate: [AuthGuard] },
   {
-    path: "Paises",
-    component: ListadoDePaisesComponent,
-    canActivate: [AuthGuard]
+    path: 'Listado', component: ListadoDeResultadosComponent,
+    canActivate: [AuthGuardService],
+    data: { animation: 'Contact' }
   },
-
   {
-    path: "Juegos",
+    path: 'Paises', component: ListadoDePaisesComponent,
+    canActivate: [AuthGuardService],
+    data: { animation: 'Contact' }
+  },
+  {
+    path: 'Jugadores', component: JugadoresListadoComponent,
+    canActivate: [AuthGuardService],
+    data: { animation: 'Contact' }
+  },
+  {
+    path: 'Juegos',
     component: JuegosComponent,
-    children: [
-      { path: "", component: MenuCardComponent },
-      {
-        path: "Adivina",
-        component: AdivinaElNumeroComponent,
-        canActivate: [AuthGuard]
-      },
-      {
-        path: "AdivinaMasListado",
-        component: AdivinaMasListadoComponent,
-        canActivate: [AuthGuard]
-      },
-      {
-        path: "AgilidadaMasListado",
-        component: AgilidadMasListadoComponent,
-        canActivate: [AuthGuard]
-      },
-      {
-        path: "Agilidad",
-        component: AgilidadAritmeticaComponent,
-        canActivate: [AuthGuard]
-      }
-    ]
+    canActivate: [AuthGuardService],
+    data: { animation: 'Contact' },
+    children:
+      [{ path: '', component: MenuCardComponent },
+      /*{ path: 'Flippisay', component: FlippisayComponent },
+      { path: 'Anagrama', component: AnagramaComponent },
+      { path: 'PiedraPapelTijera', component: PiedraPapelTijeraComponent },*/
+      { path: 'Agilidad', component: AgilidadAritmeticaComponent },
+      { path: 'Adivina', component: AdivinaElNumeroComponent },
+      //{ path: 'Tateti', component: TatetiComponent },
+
+      { path: 'AdivinaMasListado', component: AdivinaMasListadoComponent },
+      { path: 'AgilidadaMasListado', component: AgilidadMasListadoComponent }]
   },
-  { path: "**", component: ErrorComponent },
-  { path: "error", component: ErrorComponent }
-];
+  { path: '**', component: ErrorComponent },
+  { path: 'error', component: ErrorComponent }];
 
 @NgModule({
-  imports: [RouterModule.forRoot(MiRuteo)],
-  exports: [RouterModule]
+  imports: [
+    RouterModule.forRoot(MiRuteo)
+  ],
+  exports: [
+    RouterModule
+  ]
 })
 export class RuteandoModule { }
