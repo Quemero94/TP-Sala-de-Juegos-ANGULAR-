31e24026ce0e363fbbf590e4041ffecd68909827
Quemero94 <matiaserco@gmail.com>
2020-05-07 02:34:03 -0300

    adivina y arit logica

--- src/app/componentes/agilidad-aritmetica/agilidad-aritmetica.component.ts
+++ src/app/componentes/agilidad-aritmetica/agilidad-aritmetica.component.ts
@@ -1,50 +1,42 @@
 import { Component, OnInit, Input, Output, EventEmitter } from '@angular/core';
 import { JuegoAgilidad } from '../../clases/juego-agilidad'
-
 import { Subscription } from "rxjs";
 import { TimerObservable } from "rxjs/observable/TimerObservable";
+import { FirebaseService } from '../../servicios/firebase.service';
+
 @Component({
   selector: 'app-agilidad-aritmetica',
   templateUrl: './agilidad-aritmetica.component.html',
-  styleUrls: ['./agilidad-aritmetica.component.css']
+  styleUrls: ['./agilidad-aritmetica.component.scss']
 })
+
 export class AgilidadAritmeticaComponent implements OnInit {
-  @Output()
-  enviarJuego: EventEmitter<any> = new EventEmitter<any>();
+  @Output() enviarJuego: EventEmitter<any> = new EventEmitter<any>();
   nuevoJuego: JuegoAgilidad;
-  ocultarVerificar: boolean;
-  Tiempo: number;
-  repetidor: any;
-  private subscription: Subscription;
+
+
   ngOnInit() {
   }
-  constructor() {
-    this.ocultarVerificar = true;
-    this.Tiempo = 5;
+
+  constructor(public firebaseService: FirebaseService) {
     this.nuevoJuego = new JuegoAgilidad();
+    this.newGame();
     console.info("Inicio agilidad");
   }
-  NuevoJuego() {
-    this.ocultarVerificar = false;
-    this.repetidor = setInterval(() => {
-
-      this.Tiempo--;
-      console.log("llego", this.Tiempo);
-      if (this.Tiempo == 0) {
-        clearInterval(this.repetidor);
-        this.verificar();
-        this.ocultarVerificar = true;
-        this.Tiempo = 5;
-      }
-    }, 900);
-
-  }
-  verificar() {
-    this.ocultarVerificar = false;
-    clearInterval(this.repetidor);
 
 
 
+  newGame() {
+    if (this.nuevoJuego.scorePlayer > 0) {
+      this.loadResult();
+    }
+    this.nuevoJuego.reloadPage();
   }
 
+  loadResult() {
+    this.firebaseService.addResult('AgilidadAritmetica', this.nuevoJuego.scorePlayer, (this.nuevoJuego.lifePlayer > 0))
+      .then(result => {
+        console.log("insert result");
+      });
+  }
 }
